2d76bb358be20e3509c6622f58cca5067a93dda3
thatgalcalledaisha <muiliaishat87@gmail.com>
2023-05-22 12:58:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-*main - entry point
-*/
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual pzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
